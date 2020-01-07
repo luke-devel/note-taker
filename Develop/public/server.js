8e36dd7131db015aea081efc720298d9d0f1fd1f
luke-ols0 <luke.olson@outlook.com>
2020-01-07 15:00:57 -0600

    pusshing files with working express server

---
+++ Develop/public/server.js
@@ -0,0 +1,52 @@
+// Dependencies
+// =============================================================
+var express = require("express");
+var path = require("path");
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+// Routes
+// =============================================================
+
+// Basic route that sends the user first to the AJAX Page
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "/index.html"));
+});
+
+app.get("/notes", function (req, res) {
+  res.sendFile(path.join(__dirname, "/notes.html"));
+});
+// // Displays all characters
+// app.get("/api/characters", function (req, res) {
+//   return res.json(characters);
+// });
+
+// // Displays a single character, or returns false
+// app.get("/api/characters/:character", function (req, res) {
+//   var chosen = req.params.character;
+
+//   console.log(chosen);
+
+//   for (var i = 0; i < characters.length; i++) {
+//     if (chosen === characters[i].routeName) {
+//       return res.json(characters[i]);
+//     }
+//   }
+
+//   return res.json(false);
+// });
+
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function () {
+  console.log("App listening on PORT " + PORT);
+});
